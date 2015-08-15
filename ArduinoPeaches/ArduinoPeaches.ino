32e00397c70877e804842d0a6ae2e92871556a41
Jonathan Uesato <juesato@mit.edu>
2015-08-15 16:04:16 -0400

    Moving in code

---
+++ ArduinoPeaches/ArduinoPeaches.ino
@@ -0,0 +1,102 @@
+#include <CapacitiveSensor.h>
+//#include <MIDI.h>
+//#include <peaches.ino>
+#define NPEACHES 10
+
+#define TIMEOUT 10
+
+boolean debug = false;
+
+//MIDI_CREATE_DEFAULT_INSTANCE();
+
+//Inputs 
+const int txPin = 2;
+const int rxPins[NPEACHES] = {3,4,5,6,7,8,9,10,11,12};
+CapacitiveSensor *peaches[NPEACHES];
+
+int samples = 6;
+int debugState = 0;
+int currentState;
+int ledPin = 13;
+
+void setup() {
+  if(debug == false) {
+//    MIDI.begin();
+  } 
+  else {
+    Serial.begin(9600);
+  }
+  pinMode(txPin,OUTPUT);
+  pinMode(ledPin, OUTPUT); //debug
+
+  // Set peaches as CapacitiveSensors
+  for (int i = 0; i < NPEACHES; i++) {
+    peaches[i] = new CapacitiveSensor(txPin, rxPins[i]);
+  }  
+  //Set timeouts
+  for (int i = 0; i < NPEACHES; i++) {
+    peaches[i] -> set_CS_Timeout_Millis(TIMEOUT);
+    //peaches[i] -> set_CS_AutocaL_Millis(0xFFFFFFFF);
+  }
+}
+
+void loop() {
+  getThreshold();
+  readRetrigger();
+  int reading[10];
+  //Get peach
+  for (int i = 0; i < NPEACHES; i++) {
+    reading[i] = peaches[i] -> capacitiveSensor(samples);
+  }
+//  int r1 =  p1.capacitiveSensor(samples);
+
+  if(debug == false) {
+    //peachRead(int number, int reading, int midiNote, int midiChannel)
+    for (int i = 0; i < NPEACHES; i++) {
+      if (i == 0) {
+        long pls = peaches[i] -> capacitiveSensorRaw(samples); 
+        //Serial.println(pls);  
+        //Serial.print(" " );
+        //Serial.println(reading[i]);
+      }
+      peachRead(i, reading[i], i, 2); 
+    }
+//    peachRead(0, r1, 0, 2);
+//    peachRead(1, r2, 1, 2);
+//    peachRead(2, r3, 2, 2);
+//    peachRead(3, r4, 3, 2);
+//
+//    peachRead(4, r5, 4, 2);
+//    peachRead(5, r6, 5, 2);
+//    peachRead(6, r7, 6, 2);
+//    peachRead(7, r8, 7, 2);
+//
+//    peachRead(8, r9, 8, 2);
+//    peachRead(9, r10, 9, 2);
+//    peachRead(10, r11, 0, 3);
+//    peachRead(11, r12, 0, 16);
+//
+//    peachRead(12, r13, 1, 16);
+//    peachRead(13, r14, 13, 4);
+//    peachRead(14, r15, 17, 4);
+//    peachRead(15, r16, 18, 4);
+//
+//    peachRead(16, r17, 7, 6);
+//    peachRead(17, r18, 8, 6);
+//    peachRead(18, r19, 10, 6);
+//    peachRead(19, r20, 10, 5);
+//
+//    peachRead(20, r21, 10, 1);
+//    peachRead(21, r22, 12, 1);
+//    peachRead(22, r23, 14, 1);
+//    peachRead(23, r24, 15, 1);
+//
+//    peachRead(24, r25, 17, 1);
+//    peachRead(25, r26, 19, 1);
+//    peachRead(26, r27, 0, 7);
+  } 
+  else {
+    delay(500);
+  }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# BeetMachine
-beat machine made out of beets
+# Beet Machine
+
+A beat machine made out of beets. Based on the code for the [peach machine](https://www.youtube.com/watch?v=C7NaFOBrhP8).
+
+[DEMO]
+
+`ArduinoPeaches` contains the Arduino code to detect touch. `PeachesSoundboard` contains a Python script that communicates with the Arduino using the [pyserial](http://pyserial.sourceforge.net/pyserial_api.html) library. 
+
+This hack relies on the Arduino [CapacitiveSensor](http://playground.arduino.cc/Main/CapacitiveSensor) library, which also has a decent explanation of the underlying capacitive sensing technology for detecting touches.
+
+## Setup
+
+ 1. Clone the repository
+ 2. Arrange Arduino on breadboard as explained in the Breadboard section.
+ 3. Upload `ArduinoPeaches.ino` to Arduino through USB.  You likely will have to add the Arduino [CapacitiveSensor](http://playground.arduino.cc/Main/CapacitiveSensor) library first.
+ 4. Plug in Arduino through USB. 
+ 5. Run `python soundboard.py [PORT]` with the appropriate serial port. This should look something like `/dev/ttyACM1` on Linux, or `COM1` on Windows.
+ 6. You should be able to make beats by pinching the lead wires now.
+
+
+### Dependencies
+
+Arduino:
+
+Add [CapacitiveSensor](http://playground.arduino.cc/Main/CapacitiveSensor) library.
+
+Python:
+```
+juesato@juesato:~$ sudo apt-get install python-pyaudio
+juesato@juesato:~$ sudo apt-get install python-pygame
+```
+
+## Breadboard
+
+Here is a picture of my breadboard. 
+
+[PICTURE]
+
+in 2 is the send pin, which is hooked up to the 5V rail so that it's easy to connect to many receive pins at once. The current goes through a ~200 kOhm resistor to the receive pin. 
+
+After the resistor is a wire with the other end attached to a beet, which can be touched to trigger the sensor. You can also pinch the wires directly to trigger them.
+
+## Sample Tracks
+
+Hopefully I'll get around to making some arrangements which can be played with this.
